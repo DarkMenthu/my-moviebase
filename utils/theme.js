9c2c7ca6544d8b4f56a1d490b2f43a12da73d2d6
DarkMenthu <idascalu@outlook.com>
2022-01-10 22:53:43 +0200

    initial commit

---
+++ utils/theme.js
@@ -0,0 +1,18 @@
+
+import { extendTheme } from '@chakra-ui/react';
+
+const config = {
+  initialColorMode: 'dark',
+};
+
+const components = {
+  Container: {
+    baseStyle: {
+      maxW: 'container.xl',
+    },
+  },
+};
+
+const theme = extendTheme({ config, components });
+
+export default theme;
